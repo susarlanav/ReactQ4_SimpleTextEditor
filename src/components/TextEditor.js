14076774923360ef66eb8f2a84b3ae44cb47a14a
susarlanav <navyasusarla@gmail.com>
2020-09-11 17:04:42 -0500

    commit3

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -2,15 +2,57 @@ import React from 'react';
 
 class TextEditor extends React.Component {
 
+constructor(){
+  super();
+  this.state={
+    term:"",
+    listterms:[]
+  }
+}
+
+
+handleChange=(event)=>{
+  this.setState({
+    term: event.target.value,
+  })
+}
+
+appendText=()=>{
+  let list = this.state.listterms;
+  list.push(this.state.term+'');
+  this.setState({
+    listterms : list
+  })
+  this.setState({
+    term:"",
+  })
+}
+
+
+undoChange=()=>{
+  let list = this.state.listterms;
+  list.pop(''+this.state.term);
+  this.setState({
+    listterms : list
+  })
+ this.setState({term: ''});
+}
+
+
   render() {
     return (
       <React.Fragment>
         <div className="controls">
-          <input className="word-input" type="text" data-testid="word-input" />
-          <button data-testid="append-button">Append</button>
-          <button data-testid="undo-button">Undo</button>
+          <input
+          value={this.state.term}
+          className="word-input"
+          type="text"
+          onChange={this.handleChange}
+          data-testid="word-input" />
+          <button data-testid="append-button" disabled={this.state.input===''} onClick={this.appendText}>Append</button>
+          <button data-testid="undo-button" onClick={this.undoChange}>Undo</button>
         </div>
-        <div className="text-field" data-testid="text-field">REPLACE ME WITH CURRENT TEXT CONTENT</div>
+        <div className="text-field" data-testid="text-field">{this.state.listterms}</div>
       </React.Fragment>
     );
   }
