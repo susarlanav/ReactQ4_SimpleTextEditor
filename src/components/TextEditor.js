b8ee96dcb59c2b804c5fb653854c372db5cb87c5
susarlanav <navyasusarla@gmail.com>
2020-09-11 18:25:23 -0500

    commit4

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,58 +1,41 @@
 import React from 'react';
 
+
 class TextEditor extends React.Component {
-
-constructor(){
-  super();
-  this.state={
-    term:"",
-    listterms:[]
+  constructor(){
+    super();
+    this.state={
+      input:'',
+      inputList:[]
+    }
   }
-}
+  handleAppend=()=>{
+    let list = this.state.inputList;
+    list.push(' ' +this.state.input);
+    this.setState({inputList:list});
+    this.setState({input: ''});
 
+  }
+  handleUndo=()=>{
+    let list = this.state.inputList;
+    list.pop(this.state.input + ' ');
+    this.setState({inputList:list});
+    this.setState({input: ''});
+  }
 
-handleChange=(event)=>{
-  this.setState({
-    term: event.target.value,
-  })
-}
-
-appendText=()=>{
-  let list = this.state.listterms;
-  list.push(this.state.term+'');
-  this.setState({
-    listterms : list
-  })
-  this.setState({
-    term:"",
-  })
-}
-
-
-undoChange=()=>{
-  let list = this.state.listterms;
-  list.pop(''+this.state.term);
-  this.setState({
-    listterms : list
-  })
- this.setState({term: ''});
-}
-
+  onInputChange=(event)=>{
+    this.setState({input: event.target.value});
+  }
 
   render() {
     return (
       <React.Fragment>
         <div className="controls">
-          <input
-          value={this.state.term}
-          className="word-input"
-          type="text"
-          onChange={this.handleChange}
-          data-testid="word-input" />
-          <button data-testid="append-button" disabled={this.state.input===''} onClick={this.appendText}>Append</button>
-          <button data-testid="undo-button" onClick={this.undoChange}>Undo</button>
+          <input value ={this.state.input} onChange={this.onInputChange} id="inputBox" className="word-input" type="text" data-testid="word-input" />
+          <button data-testid="append-button" onClick={this.handleAppend} disabled={this.state.input===''}>Append</button>
+          <button data-testid="undo-button" onClick={this.handleUndo} disabled={this.state.inputList.length === 0}>Undo</button>
         </div>
-        <div className="text-field" data-testid="text-field">{this.state.listterms}</div>
+    <div className="text-field" data-testid="text-field">{this.state.inputList}</div>
       </React.Fragment>
     );
   }
